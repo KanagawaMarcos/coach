bb9c7d7248cd2858a5921c4544f874ab4ef50372
Marcos Costa <Marcos Costa>
2021-02-28 17:36:02 +0000

    feat: try to build simple treadmill parser

--- src/parser/Library.fsx
+++ src/parser/Library.fsx
@@ -1,9 +1,22 @@
 ﻿#r "nuget: FParsec, 1.1.1"
 
+open FParsec
+
 type Exercise = 
     | Curl
     | Pulley
+    | Walk
 
 type Equipement = 
     | Smith
     | Cross
+    | Treadmill
+
+let pworkout = pstring "treadmill" >>% Treadmill
+
+let workout input =
+    match run pworkout input with
+        | Success(res, _, _) -> Result.Ok res
+        | Failure(err, _, _) -> Result.Error err
+
+workout "treadmill" |> printf "%O"
