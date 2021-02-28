96191102e0bed4f5dae4899aa71d6ac0edba5555
Marcos Costa <Marcos Costa>
2021-02-28 18:17:40 +0000

    feat: add repetitions to increase domain description

--- src/parser/Workout.fsx
+++ src/parser/Workout.fsx
@@ -11,12 +11,22 @@ type Equipement =
     | Smith
     | Cross
     | Treadmill
+    | None
 
-let pworkout = pstring "treadmill" >>% Treadmill
+type Rep = {
+    Exercise: Exercise;
+    Equipement: Equipement;
+}
+
+let pworkout =
+    choice [
+        stringReturn "Treadmill" { Exercise=Walk; Equipement=Treadmill}
+        stringReturn "Walk"  {Exercise=Walk; Equipement= None}
+    ]
 
 let workout input =
     match run pworkout input with
         | Success(res, _, _) -> Result.Ok res
         | Failure(err, _, _) -> Result.Error err
 
-workout "Treadmill"
+workout "Walks"
